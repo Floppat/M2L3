df48f62a51261b767fa60ca490087d4902c76d6a
Floppat <turtleprogram33@gmai.com>
2025-02-15 17:38:13 +0200

    working quiz

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-
 import telebot
+from collections import defaultdict
+
 from config import token
-# Задание 7 - испортируй команду defaultdict
 from logic import quiz_questions
 
-user_responses = {} 
-# Задание 8 - создай словарь points для сохранения количества очков пользователя
 
 bot = telebot.TeleBot(token)
 
+
+user_responses = {} 
+points = defaultdict(int)
+
+
 def send_question(chat_id):
-    bot.send_message(chat_id, quiz_questions[user_responses[chat_id]].text, reply_markup=quiz_questions[user_responses[chat_id]].gen_markup())
+    bot.send_message(chat_id, quiz_questions[user_responses[chat_id]].get_text, reply_markup=quiz_questions[user_responses[chat_id]].gen_markup())
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-
     if call.data == "correct":
         bot.answer_callback_query(call.id, "Answer is correct")
-        # Задание 9 - добавь очки пользователю за правильный ответ
+        points[call.message.chat.id] += 1
     elif call.data == "wrong":
         bot.answer_callback_query(call.id,  "Answer is wrong")
-      
-    # Задание 5 - реализуй счетчик вопросов
 
-    # Задание 6 - отправь пользователю сообщение с количеством его набранных очков, если он ответил на все вопросы, а иначе отправь следующий вопрос
-
+    user_responses[call.message.chat.id]+=1
+    
+    if user_responses[call.message.chat.id]>=len(quiz_questions):
+        bot.send_message(call.message.chat.id, f"The end, {points[call.message.chat.id]}/{user_responses[call.message.chat.id]} points")
+    else:
+        send_question(call.message.chat.id)
 
 @bot.message_handler(commands=['start'])
 def start(message):
